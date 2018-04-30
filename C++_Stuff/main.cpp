424987d93e1e293317ed9ebee09b7fcd270502d2
QPUK1337 <legojames99@gmail.com>
2018-04-30 19:36:47 +0100

    Starting Login Script 	modified:   .vscode/launch.json 	modified:   .vscode/tasks.json 	deleted:    C++_Stuff/loginScript.cpp 	modified:   C++_Stuff/main.cpp 	modified:   a.exe 	C++_Stuff/test.cpp

--- C++_Stuff/main.cpp
+++ C++_Stuff/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <time.h>
+#include <fstream>
+#include <string>
+#include <conio.h>
 
 using namespace std;
 
@@ -14,12 +17,64 @@ void sleepcp(int milliseconds) // Cross-platform sleep function
     }
 }
 
-int start() {
+string login() {
+    // TEMPORARY
+    string username;
+    username = "root";
+    string password;
+    password = "toor";
+    // TEMPORARY
+    string userAttempt;
+    string passAttempt;
+    char ch;
+    // start the login sequence
     system("cls");
+    cout << "Logging In";
+    sleepcp(1000);
+    cout << ".";
+    sleepcp(1000);
+    cout << ".";
+    sleepcp(1000);
+    cout << ".";
+    sleepcp(1000);
+    system("cls");
+    cout << "Username: ";
+    cin >> userAttempt;
+    cout << "Password: ";
+    ch = _getch();
+    while(ch != 13) {//character 13 is enter
+        passAttempt.push_back(ch);
+        cout << '*';
+        ch = _getch();
+    }
+    cout << endl;
+    if (userAttempt == username && passAttempt == password) {
+        cout << "You Successfully Logged in!";
+    }
+    else if (userAttempt == username) {
+        if (passAttempt != password) {
+            cout << "Password Incorrect";
+        }
+    }
+    else {
+        cout << "User not in my Database...";
+    }
+    sleepcp(1000);
+
+
+    return username;
+}
+
+int start() {
+    // start the actual program
+    system("cls");
+    // title
     cout << "Welcome to the JAM Login Script (C++ Edition)" << endl;
     sleepcp(1000);
     system("cls");
+    // sets weather the user wants to exit
     bool exit;
+    // allows for continuos cycling
     while(!exit) {
         string response;
         system("cls");
@@ -29,8 +84,7 @@ int start() {
         cout << endl << "Option: ";
         cin >> response;
         if(response == "1") {
-            cout << "Logging In..." << endl;
-            sleepcp(1000);
+            login();
         }
         else if(response == "2") {
             cout << "Registering..." << endl;
@@ -39,6 +93,7 @@ int start() {
         else if(response == "3") {
             exit = true;
         }
+        // error function
         else {
             cout << "I can't read this alien text you speak!" << endl;
             sleepcp(1000);
@@ -48,6 +103,9 @@ int start() {
     return 0;
 }
 
+
+
 int main() {
+    // init function
     start();
 }
