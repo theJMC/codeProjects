c8d8e3d8469a5a7ed32d5f5e28d23a374934b948
_TheJMC_ <legojames99@gmail.com>
2019-02-09 12:34:09 +0000

    Python Run

---
+++ Python_Stuffs/Inventory System/inventory.py
@@ -0,0 +1,42 @@
+"""
+Title: Inventory System
+
+Date: 09/02/2019
+
+Author: James McCarthy
+
+Notes:
+
+Todo:
+
+"""
+import random
+import getpass
+
+# Variables
+
+
+# Classes
+
+class itemReservation:
+
+    def __init__(self):
+        pass
+
+    def reserve(user):
+        pass
+
+# Functions
+
+def login():
+    userName = input("User: ")
+    return userName
+
+# Main
+
+def main():
+    login()
+
+
+if __name__ == "__main__":
+    main()
