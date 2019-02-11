dc02b188a053ba3f5b5cde459bc48e9439133f92
_TheJMC_ <legojames99@gmail.com>
2019-02-11 20:00:17 +0000

    More Inventory

--- Python_Stuffs/Inventory System/inventory.py
+++ Python_Stuffs/Inventory System/inventory.py
@@ -15,11 +15,13 @@ import random
 import getpass
 import os
 import hashlib
+import csv
+import time
 
 
 # Variables
 
-users = {0000: "Administrator", 1337: "James", 1234: ""}
+users = {0000: "Administrator", 1337: "James", 1234: "Mjeff"}
 adminPW = "c7ad44cbad762a5da0a452f9e854fdc1e0e7a52a38015f23f3eab1d80b931dd472634dfac71cd34ebc35d16ab7fb8a90c81f975113d6c7538dc69dd8de9077ec"
 __user__ = ""
 exit = False
@@ -34,7 +36,7 @@ class item:
         self.name = name
         self.location = location
 
-    def reserve(user):
+    def reserve(self, user):
         pass
 
 # Functions
@@ -43,14 +45,20 @@ class item:
 def login():
     while True:
         userName = input("User: ")
-        if userName == "0000":
-            passAttempt = getpass.getpass()
-            if hashlib.sha512(passAttempt.encode()).hexdigest() == adminPW:
+        if userName == "EXIT":
+            raise SystemExit
+        if len(userName) == 4:
+            if userName == "0000":
+                passAttempt = getpass.getpass()
+                if hashlib.sha512(passAttempt.encode()).hexdigest() == adminPW:
+                    break
+                else:
+                    continue
+            else: 
                 break
-            else:
-                continue
-        else: 
-            break
+        else:
+            print("Invalid ID")
+            continue
     return int(userName)
 
 def addStock():
@@ -61,11 +69,31 @@ def addStock():
     stock.append(temp)
 
 def checkStock():
+    ref.clear()
+    print("Stock:\n")
     for i in range(len(stock)):
-        print(stock[i])
+        print(stock[i].name)
 
 def deleteStock():
-    pass
+    ref.clear()
+    print("Please select a number to remove")
+    for i in range(len(stock)):
+        print("[" + str(i) + "]: " + stock[i].name)
+    option = input("Select a number > ")
+    try:
+        del stock[int(option)]
+    except IndexError:
+        print("That ID is not in our database!!")
+        input("Press enter to continue")
+    except ValueError:
+        print("Please input a number only")
+        input("Press enter to continue")
+
+def lookUp():
+    itemToLookUp = input("Please enter the name or ID of an item > ")
+    if itemToLookUp == int:
+        try:
+            
 
 def reserve():
     pass
@@ -74,23 +102,23 @@ def reserve():
 
 def main():
     ref.clear()
-    __user__ = login()
     while exit == False:
         ref.clear()
         print("Inventory System: Welcome " + users[__user__])
-        print("\n[1]: Add Stock\n[2]: Check Stock\n[3]: Remove Stock\n[5]: Reserve Stock\n[0]: Exit")
+        print("\n[1]: Add Stock\n[2]: Check Stock\n[3]: Remove Stock\n[4]: Look Up Item\n[5]: Reserve Stock\n[0]: Exit")
         option = input("Please select an Option: > ")
         if option == "1":
             addStock()
-            print("Adding Stock")
             input("Press enter to continue")
         elif option == "2":
             checkStock()
-            print("Checking Stock")
             input("Press enter to continue")
         elif option == "3":
             deleteStock()
             print("Removing Stock")
+            input("Press enter to continue")
+        elif option == "4":
+            lookUp()
             input("Press enter to continue")
         elif option == "5":
             reserve()
@@ -107,4 +135,6 @@ def main():
 
 
 if __name__ == "__main__":
+    ref.clear()
+    __user__ = login()
     main()
