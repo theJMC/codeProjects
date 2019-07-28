abdd616b4437c6078d700dd71c08c63b1fb91a4f
_TheJMC_ <legojames99@gmail.com>
2019-07-28 21:10:05 +0100

    CherryPy Image Upload Site

--- Python/CherryPy/Website Upload/cherry.py
+++ Python/CherryPy/Website Upload/cherry.py
@@ -14,7 +14,7 @@ import cherrypy
 from columnar import columnar
 import time
 from Lib.datetime import datetime
-import os.path
+import os
 
 class imageServer(object):
 
@@ -26,12 +26,34 @@ class imageServer(object):
     def upload(self, image, submit):
         upload_time = datetime.now().strftime("%d_%m_%Y_%H_%M_%S_%f")
         file_extension = os.path.splitext(image.filename)[1]
-        # image_path = "C:\\Users\\legoj\\Desktop\\Code Workspace\\codeProjects\\Python\\CherryPy\\Website Upload\\image"
         img = image.file.read()
-        with open(upload_time + file_extension, "wb+") as file:
+        with open("image/" + upload_time + file_extension, "wb+") as file:
             file.write(img)
-        return "Done!"
-        # return datetime.now().strftime("%d-%m-%Y_%H:%M:%S:%f")
+        with open("upload.html", "r") as file:
+            data = file.readlines()
+            page = ''.join(data)
+        return str(page).format("<h2>Done!</h2>")
+
+    @cherrypy.expose
+    def download(self):
+        with open("download.html", "r") as file:
+            data = file.readlines()
+            page = ''.join(data)
+        os.chdir("image")
+        images = os.listdir()
+        os.chdir("..")
+        results = []
+        for item in images:
+            results.append(f"<img src=/download/{item} class='img-thumbnail'>")
+        # headers = ["Image", "Link"]
+        # table = columnar(results, headers)
+        return str(page).format("<div class='container'> " + ''.join(results) + "</div>")
 
 if __name__ == "__main__":
-    cherrypy.quickstart(imageServer())
+    config = {'/download':
+    {
+        'tools.staticdir.on': True,
+        'tools.staticdir.dir': "C:\\Users\\legoj\\Desktop\\Code Workspace\\codeProjects\\Python\\CherryPy\\Website Upload\\image",
+    }
+}
+    cherrypy.quickstart(imageServer(), config=config)
